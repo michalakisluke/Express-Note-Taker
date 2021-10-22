deb95bfc7921fe78e4670d9d79d568cfb07fd59e
Luke Michalakis <michalakisluke@gmail.com>
2021-10-22 15:44:19 -0400

    <Complete get route, build post route>

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,18 +1,13 @@
 const router = require("express").Router();
-//const {getNotes, saveNote, deleteNote} = require('../../public/assets/js/index');
 const notes = require("../../db/db.json");
 
 router.get("/api/notes", (req, res) => {
-    // let results = notes;
-    // if (req.query) {
-    //   results = getNotes(req.query, results);
-    // }
-    // res.json(results);
-    res.send('Test');
+    res.json(notes);
 });
 
 router.post("/api/notes", (req, res) => {
     console.log(req);
+    res.json(req);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
+const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
-// Include db as variable
-const data = require('./db/db.json');
-const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 //parse incoming string or array data
@@ -10,8 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 //parse incoming JSON data
 app.use(express.json());
 app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(apiRoutes);
+app.use(htmlRoutes);
 // Provide a file path to a location in the app (Public folder), and makes
 // them into static resources, meaning it can be accessed without a specific
 // endpoint
