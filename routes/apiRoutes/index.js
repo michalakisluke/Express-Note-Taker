d12bcc1b68952a86f4906577c88007ad173aa170
Luke Michalakis <michalakisluke@gmail.com>
2021-10-22 15:05:45 -0400

    <Build api routes>

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,17 +1,18 @@
 const router = require("express").Router();
-const {getNotes, saveNote, deleteNote} = require('../../public/assets/js/index');
+//const {getNotes, saveNote, deleteNote} = require('../../public/assets/js/index');
 const notes = require("../../db/db.json");
 
 router.get("/api/notes", (req, res) => {
-    let results = notes;
-    if (req.query) {
-      results = getNotes(req.query, results);
-    }
-    res.json(results);
+    // let results = notes;
+    // if (req.query) {
+    //   results = getNotes(req.query, results);
+    // }
+    // res.json(results);
+    res.send('Test');
 });
 
 router.post("/api/notes", (req, res) => {
-
+    console.log(req);
 });
 
 module.exports = router;
