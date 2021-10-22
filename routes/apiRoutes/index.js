6b028ad4981c1309023290d3093d92c3b09782db
Luke Michalakis <michalakisluke@gmail.com>
2021-10-22 16:42:13 -0400

    <Build apiRoutes>

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,13 +1,30 @@
 const router = require("express").Router();
 const notes = require("../../db/db.json");
 
+// Populate previous notes on left side of screen
 router.get("/api/notes", (req, res) => {
     res.json(notes);
 });
 
-router.post("/api/notes", (req, res) => {
-    console.log(req);
-    res.json(req);
+// Populate previous notes info on right side of screen
+router.get("/:id", (req, res) => {
+
 });
+
+// Write new note
+router.post("/api/notes", (req, res) => {
+    const note = req.body;
+    req.body.id = notes.length;
+    notes.push(note);
+    res.json(notes);
+});
+
+
+// Get text and title from note on the left
+
+// Delete a note
+// router.delete("/api/note/:id", (req, res) => {
+
+// });
 
 module.exports = router;
